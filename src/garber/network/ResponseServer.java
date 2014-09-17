94f770274a49ce7af973cd1849af3fed01563faa
ShoshanaGarber <shanagarber518@gmail.com>
2014-09-16 22:21:29 -0400

    fixed ResponseServer
    changed while loop changes

--- src/garber/network/ResponseServer.java
+++ src/garber/network/ResponseServer.java
@@ -7,26 +7,40 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
- 
+
 public class ResponseServer {
 
-	
-	public static void main (String[] args) throws IOException{
-		
-		ServerSocket serverSocket = new ServerSocket(8080);//ip is myself use port 8080-standard listening port
-		Socket socket = serverSocket.accept();//this is how you listen, when someone connects to you a socket is returned
-        
-		InputStream  in = socket.getInputStream();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-	    
-	    String line;
-	    while((line = reader.readLine())!=null){
-	    	System.out.println(line);
-	    }
-		OutputStream out = socket.getOutputStream();
-		out.write("Hello World!".getBytes());
-		out.flush();
-		out.close();
+	public static void main(String[] args) throws IOException {
+
+		ServerSocket serverSocket = new ServerSocket(8080);// ip is myself use
+															// port
+															// 8080-standard
+															// listening port
+		int counter = 0;
+		while (true) {
+			Socket socket = serverSocket.accept();// this is how you listen,
+													// when someone connects to
+													// you a socket is returned
+
+			InputStream in = socket.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+			String line;
+			while (!"".equals((line = reader.readLine()))) {
+				System.out.println(line);
+			}
+			OutputStream out = socket.getOutputStream();
+			String response = "<h2>This is the " + counter + "th request</h2>";
+			out.write("HTTP/1.1 200 OK\n".getBytes());
+			out.write("Content-Type: text/html; charset=utf-8\n".getBytes());
+			out.write(("Content-Length: " + response.length() + "\n\n").getBytes());
+			out.write(response.getBytes());
+			out.flush();
+			out.close();
+			counter++;
+
+		}
+		// this program returns the http request the browser makes when you try
+		// to connect to the server
 	}
-	//this program returns the http request the browser makes when you try to connect to the server
 }
