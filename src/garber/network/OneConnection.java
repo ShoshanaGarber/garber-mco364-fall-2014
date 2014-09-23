b30306c87ce65a39ed90e9f518a85fc7907f3e0c
ShoshanaGarber <shanagarber518@gmail.com>
2014-09-22 22:01:57 -0700

    Multi Threading
    Adjusted  Request Server to act as a multi threaded server.

---
+++ src/garber/network/OneConnection.java
@@ -0,0 +1,55 @@
+package garber.network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class OneConnection extends Thread {
+
+	private Socket socket;
+	private int counter;
+
+	public OneConnection(Socket socket, int counter) throws Exception {
+		this.socket = socket;
+		this.counter = counter;
+	}
+
+	@Override
+	public void run() {
+
+		try {
+			InputStream in = socket.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+			String line;
+			while (!"".equals((line = reader.readLine()))) {
+				System.out.println(line);
+			}
+			OutputStream out = socket.getOutputStream();
+			String response;
+			if (counter == 1) {
+				response = "<h2>This is the " + counter + "st request</h2>";
+			} else if (counter == 2) {
+				response = "<h2>This is the " + counter + "nd request</h2>";
+			} else if (counter == 3) {
+				response = "<h2>This is the " + counter + "rd request</h2>";
+			} else {
+				response = "<h2>This is the " + counter + "th request</h2>";
+			}
+			out.write("HTTP/1.1 200 OK\n".getBytes());
+			out.write("Content-Type: text/html; charset=utf-8\n".getBytes());
+			out.write(("Content-Length: " + response.length() + "\n\n").getBytes());
+			out.write(response.getBytes());
+			out.flush();
+			out.close();
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
--- src/garber/network/ResponseServer.java
+++ src/garber/network/ResponseServer.java
@@ -10,7 +10,7 @@ import java.net.Socket;
 
 public class ResponseServer {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 
 		ServerSocket serverSocket = new ServerSocket(8080);// ip is myself use
 															// port
@@ -21,26 +21,15 @@ public class ResponseServer {
 			Socket socket = serverSocket.accept();// this is how you listen,
 													// when someone connects to
 													// you a socket is returned
-
-			InputStream in = socket.getInputStream();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
-			String line;
-			while (!"".equals((line = reader.readLine()))) {
-				System.out.println(line);
-			}
-			OutputStream out = socket.getOutputStream();
-			String response = "<h2>This is the " + counter + "th request</h2>";
-			out.write("HTTP/1.1 200 OK\n".getBytes());
-			out.write("Content-Type: text/html; charset=utf-8\n".getBytes());
-			out.write(("Content-Length: " + response.length() + "\n\n").getBytes());
-			out.write(response.getBytes());
-			out.flush();
-			out.close();
 			counter++;
+			OneConnection newConnection = new OneConnection(socket, counter);
+
+			newConnection.start();
+			
+			// this program returns the http request the browser makes when you
+			// try
+			// to connect to the server
 
 		}
-		// this program returns the http request the browser makes when you try
-		// to connect to the server
 	}
 }
